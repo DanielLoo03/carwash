f392a74677c765b55a4d4247674d15a87dac71d3
DanielLoo03 <dloog23@gmail.com>
2025-04-17 16:55:16 -0700

    Creación de capa de presentación
    Se crea la capa de presentación con los Forms Autenticación, Toast y la clase ValidacionesUI

--- carwash/persistencia/obj/Debug/net8.0-windows/persistencia.AssemblyInfo.cs
+++ carwash/persistencia/obj/Debug/net8.0-windows/persistencia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("persistencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddb1f602db634399bb88179559bcd5769c7e042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7db93115ed956c3e32896ed1247d81040ac03c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("persistencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("persistencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
