98049fbccbc84f640c66cd495043e9af343f7e44
DanielLoo03 <dloog23@gmail.com>
2025-04-25 23:26:20 -0700

    PBI-EMP-HU2 | Codificación en la capa negocios
    Se agregan métodos para consultar la tabla de empleados dados de alta en las clases EmpleadosService y LogicaNegocios

--- carwash/presentacion/obj/Debug/net8.0-windows/presentacion.AssemblyInfo.cs
+++ carwash/presentacion/obj/Debug/net8.0-windows/presentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a12b77c7f468c0d94b9c046ed99d7f1182101eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9d1748d479333f2d3a3010fc09d9480ccc6a46")]
 [assembly: System.Reflection.AssemblyProductAttribute("presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
