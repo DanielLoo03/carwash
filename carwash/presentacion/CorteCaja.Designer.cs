baf05196d0d0ed8b6a2d83835f3be66ca170f56d
DanielLoo03 <dloog23@gmail.com>
2025-05-28 23:02:12 -0700

    PBI-CC-HU2 | Codificar los componentes de las interfaces de usuario
    Se agregan componentes para consultar de corte al Form CorteCaja sin codificar funcionalidad

--- carwash/presentacion/CorteCaja.Designer.cs
+++ carwash/presentacion/CorteCaja.Designer.cs
@@ -34,11 +34,21 @@
             btnRealizarCorte = new Button();
             lblCorte = new Label();
             pnlContenido = new Panel();
-            pnlBorde = new Panel();
+            dtFecha = new DateTimePicker();
+            imgFecha = new PictureBox();
+            txtAdmin = new TextBox();
+            txtDiferencia = new TextBox();
+            txtCalculado = new TextBox();
+            lblDiaCorte = new Label();
+            lblAdmin = new Label();
+            lblDiferencia = new Label();
+            lblCalculado = new Label();
+            lblContado = new Label();
+            lblDatosCorte = new Label();
             txtContado = new TextBox();
             pnlCabecera.SuspendLayout();
             pnlContenido.SuspendLayout();
-            pnlBorde.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)imgFecha).BeginInit();
             SuspendLayout();
             // 
             // pnlCabecera
@@ -47,7 +57,7 @@
             pnlCabecera.Controls.Add(lblCaja);
             pnlCabecera.Controls.Add(btnRealizarCorte);
             pnlCabecera.Controls.Add(lblCorte);
-            pnlCabecera.Location = new Point(0, 0);
+            pnlCabecera.Location = new Point(1, 0);
             pnlCabecera.Name = "pnlCabecera";
             pnlCabecera.Size = new Size(1820, 100);
             pnlCabecera.TabIndex = 3;
@@ -57,7 +67,7 @@
             lblCaja.AutoSize = true;
             lblCaja.Font = new Font("Inter", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblCaja.ForeColor = Color.FromArgb(63, 114, 175);
-            lblCaja.Location = new Point(211, 34);
+            lblCaja.Location = new Point(209, 34);
             lblCaja.Name = "lblCaja";
             lblCaja.Size = new Size(98, 44);
             lblCaja.TabIndex = 7;
@@ -67,7 +77,7 @@
             // 
             btnRealizarCorte.BackColor = Color.FromArgb(63, 114, 175);
             btnRealizarCorte.FlatStyle = FlatStyle.Popup;
-            btnRealizarCorte.Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRealizarCorte.Font = new Font("Nacelle", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnRealizarCorte.ForeColor = Color.White;
             btnRealizarCorte.Image = (Image)resources.GetObject("btnRealizarCorte.Image");
             btnRealizarCorte.ImageAlign = ContentAlignment.MiddleLeft;
@@ -95,26 +105,125 @@
             // 
             pnlContenido.BackColor = SystemColors.Window;
             pnlContenido.BorderStyle = BorderStyle.FixedSingle;
-            pnlContenido.Controls.Add(pnlBorde);
-            pnlContenido.Location = new Point(0, 101);
+            pnlContenido.Controls.Add(dtFecha);
+            pnlContenido.Controls.Add(imgFecha);
+            pnlContenido.Controls.Add(txtAdmin);
+            pnlContenido.Controls.Add(txtDiferencia);
+            pnlContenido.Controls.Add(txtCalculado);
+            pnlContenido.Controls.Add(lblDiaCorte);
+            pnlContenido.Controls.Add(lblAdmin);
+            pnlContenido.Controls.Add(lblDiferencia);
+            pnlContenido.Controls.Add(lblCalculado);
+            pnlContenido.Controls.Add(lblContado);
+            pnlContenido.Controls.Add(lblDatosCorte);
+            pnlContenido.Controls.Add(txtContado);
+            pnlContenido.Location = new Point(1, 101);
             pnlContenido.Name = "pnlContenido";
-            pnlContenido.Size = new Size(1820, 547);
+            pnlContenido.Size = new Size(1820, 545);
             pnlContenido.TabIndex = 4;
             // 
-            // pnlBorde
+            // dtFecha
             // 
-            pnlBorde.BorderStyle = BorderStyle.FixedSingle;
-            pnlBorde.Controls.Add(txtContado);
-            pnlBorde.Location = new Point(76, 169);
-            pnlBorde.Name = "pnlBorde";
-            pnlBorde.Size = new Size(1669, 321);
-            pnlBorde.TabIndex = 13;
+            dtFecha.Location = new Point(1442, 97);
+            dtFecha.Name = "dtFecha";
+            dtFecha.Size = new Size(332, 31);
+            dtFecha.TabIndex = 25;
+            // 
+            // imgFecha
+            // 
+            imgFecha.Image = (Image)resources.GetObject("imgFecha.Image");
+            imgFecha.Location = new Point(1528, 57);
+            imgFecha.Name = "imgFecha";
+            imgFecha.Size = new Size(24, 24);
+            imgFecha.TabIndex = 24;
+            imgFecha.TabStop = false;
+            // 
+            // txtAdmin
+            // 
+            txtAdmin.Location = new Point(1123, 295);
+            txtAdmin.Name = "txtAdmin";
+            txtAdmin.Size = new Size(227, 31);
+            txtAdmin.TabIndex = 23;
+            // 
+            // txtDiferencia
+            // 
+            txtDiferencia.Location = new Point(860, 295);
+            txtDiferencia.Name = "txtDiferencia";
+            txtDiferencia.Size = new Size(227, 31);
+            txtDiferencia.TabIndex = 22;
+            // 
+            // txtCalculado
+            // 
+            txtCalculado.Location = new Point(596, 295);
+            txtCalculado.Name = "txtCalculado";
+            txtCalculado.Size = new Size(227, 31);
+            txtCalculado.TabIndex = 21;
+            // 
+            // lblDiaCorte
+            // 
+            lblDiaCorte.AutoSize = true;
+            lblDiaCorte.Font = new Font("Nacelle", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDiaCorte.Location = new Point(1569, 57);
+            lblDiaCorte.Name = "lblDiaCorte";
+            lblDiaCorte.Size = new Size(109, 22);
+            lblDiaCorte.TabIndex = 20;
+            lblDiaCorte.Text = "Día de corte";
+            // 
+            // lblAdmin
+            // 
+            lblAdmin.AutoSize = true;
+            lblAdmin.Font = new Font("Nacelle", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAdmin.Location = new Point(1173, 258);
+            lblAdmin.Name = "lblAdmin";
+            lblAdmin.Size = new Size(127, 22);
+            lblAdmin.TabIndex = 19;
+            lblAdmin.Text = "Administrador";
+            // 
+            // lblDiferencia
+            // 
+            lblDiferencia.AutoSize = true;
+            lblDiferencia.Font = new Font("Nacelle", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDiferencia.Location = new Point(928, 258);
+            lblDiferencia.Name = "lblDiferencia";
+            lblDiferencia.Size = new Size(93, 22);
+            lblDiferencia.TabIndex = 18;
+            lblDiferencia.Text = "Diferencia";
+            // 
+            // lblCalculado
+            // 
+            lblCalculado.AutoSize = true;
+            lblCalculado.Font = new Font("Nacelle", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCalculado.Location = new Point(666, 258);
+            lblCalculado.Name = "lblCalculado";
+            lblCalculado.Size = new Size(93, 22);
+            lblCalculado.TabIndex = 17;
+            lblCalculado.Text = "Calculado";
+            // 
+            // lblContado
+            // 
+            lblContado.AutoSize = true;
+            lblContado.Font = new Font("Nacelle", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblContado.Location = new Point(397, 258);
+            lblContado.Name = "lblContado";
+            lblContado.Size = new Size(82, 22);
+            lblContado.TabIndex = 16;
+            lblContado.Text = "Contado";
+            // 
+            // lblDatosCorte
+            // 
+            lblDatosCorte.AutoSize = true;
+            lblDatosCorte.Font = new Font("Inter", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDatosCorte.Location = new Point(697, 70);
+            lblDatosCorte.Name = "lblDatosCorte";
+            lblDatosCorte.Size = new Size(308, 49);
+            lblDatosCorte.TabIndex = 15;
+            lblDatosCorte.Text = "Datos de corte";
             // 
             // txtContado
             // 
-            txtContado.Location = new Point(38, 43);
+            txtContado.Location = new Point(329, 295);
             txtContado.Name = "txtContado";
-            txtContado.Size = new Size(150, 31);
+            txtContado.Size = new Size(227, 31);
             txtContado.TabIndex = 14;
             // 
             // CorteCaja
@@ -132,8 +241,8 @@
             pnlCabecera.ResumeLayout(false);
             pnlCabecera.PerformLayout();
             pnlContenido.ResumeLayout(false);
-            pnlBorde.ResumeLayout(false);
-            pnlBorde.PerformLayout();
+            pnlContenido.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)imgFecha).EndInit();
             ResumeLayout(false);
         }
 
@@ -148,7 +257,17 @@
         private Button btnRealizarCorte;
         private Label lblCorte;
         private Panel pnlContenido;
-        private Panel pnlBorde;
+        private TextBox txtAdmin;
+        private TextBox txtDiferencia;
+        private TextBox txtCalculado;
+        private Label lblDiaCorte;
+        private Label lblAdmin;
+        private Label lblDiferencia;
+        private Label lblCalculado;
+        private Label lblContado;
+        private Label lblDatosCorte;
         private TextBox txtContado;
+        private DateTimePicker dtFecha;
+        private PictureBox imgFecha;
     }
 }
